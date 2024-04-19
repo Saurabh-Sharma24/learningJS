3f249f06fc8945ac74d4d1af91d424ef3f289571
Saurabh Sharma <165271762+Saurabh-Sharma24@users.noreply.github.com>
2024-04-19 17:01:36 +0000

    type conversion

--- 01-basics/02-datatypes.js
+++ 01-basics/02-datatypes.js
@@ -6,3 +6,15 @@
 console.log(typeof null) // => objects
 console.log(typeof NaN) // => number
 console.log(typeof undefined) // => undefined
+
+let score = 'saurabh'
+console.log(Number (score))
+console.log(typeof Number(score))
+console.log(String(score))
+console.log(typeof String(score))
+
+// "12sa" => NaN
+// true = 1 and strings, any other number except 0 
+// and false = 0 and empty string
+// " "=> false
+// "saurabh" => true
