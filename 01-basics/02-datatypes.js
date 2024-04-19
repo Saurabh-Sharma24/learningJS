7af1cbb799a9947b36c4c50dab91a827d528bd0c
Saurabh Sharma <165271762+Saurabh-Sharma24@users.noreply.github.com>
2024-04-19 16:27:46 +0000

    02-datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all js code as newer version
+// null => empty
+// undefined => not assign yet
+// symbol => unique
+
+console.log(typeof null) // => objects
+console.log(typeof NaN) // => number
+console.log(typeof undefined) // => undefined
