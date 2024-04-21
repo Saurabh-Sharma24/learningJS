efa1c53724778f4fb59f9c0ecd6133dc940da2e0
Saurabh Sharma <165271762+Saurabh-Sharma24@users.noreply.github.com>
2024-04-21 12:18:43 +0000

    strings-most used methods

--- 01-basics/04-strings.js
+++ 01-basics/04-strings.js
@@ -1,1 +1,16 @@
 // go through string most used methods 
+
+// some commonly used string methods in JavaScript that are widely used in the industry: result by chatgpt
+
+const str = "Saurabh";
+
+console.log(str.charAt(2)) // return the character at a specified index
+console.log((str.concat(' Sharma'))) // concate the given string with specified string and return a new string
+console.log(str.indexOf('S')) // returns the index of first occurrence of specified character
+console.log((str.slice(0,5))) // slice the given string and reeturns the new string without modifying the actual string
+console.log(str.substring(1,8)) // extract the character between two specified indices
+console.log(str.split("a")) // returns a an array after dividing given string into substrings
+console.log(str.trim()) // removes white spaces from end and start
+console.log(str.replace('bh', 'v')) // replaces specified value with other value
+console.log(str.startsWith('S')) // checks whether a string starts with specified value
+console.log(str.endsWith('v')) // checks whether a string ends with specified value
