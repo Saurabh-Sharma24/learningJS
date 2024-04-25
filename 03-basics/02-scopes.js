00c13810e38cebf37a6620116163d3fee81bb055
Saurabh Sharma <165271762+Saurabh-Sharma24@users.noreply.github.com>
2024-04-25 07:10:02 +0000

    regular function vs function expression

--- 03-basics/02-scopes.js
+++ 03-basics/02-scopes.js
@@ -1,4 +1,4 @@
-var a = 1; // global scope
+var a = 1; // global scope; global scope variable can be accessed anywhere in code
 
 if(true){
     var a = 2; // a is modified
@@ -6,8 +6,20 @@ if(true){
     let c = 4;
 }
 
-console.log(a) //(modified a) var can be accessed outside block scope
+// console.log(a) //(modified a) var can be accessed outside block scope
 // console.log(b) // const can not be accessed outside of block scope
 // console.log(c) // let can not be accessed outside of block scope
 
 // global scope is different for windows(browser) amd for node
+
+
+
+// console.log(add(2,3)) // regular function can be called before there initialisation
+function add(num1,num2){ // regular function
+    return num1+num2
+}
+
+// console.log(sub(12,5)) // function expression can not be accessed before initialisation
+const sub = function(num1,num2){ // function expression
+    return num1-num2;
+}
