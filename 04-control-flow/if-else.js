2c79b232407c98f517637a1da2c1258b226b5571
Saurabh Sharma <165271762+Saurabh-Sharma24@users.noreply.github.com>
2024-04-28 17:05:30 +0000

    if else

---
+++ 04-control-flow/if-else.js
@@ -0,0 +1,16 @@
+const userName = 'Saurabh';
+if(userName){
+    console.log(`Hello, ${userName}`)
+}else{
+    console.log(`Who are you?`)
+}
+
+const balance = 1000;
+
+if(balance < 1000){
+    console.log('less')
+}else if(balance>1000){
+    console.log('high');
+}else{
+    console.log(`Equal`)
+}
