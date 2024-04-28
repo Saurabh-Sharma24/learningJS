7592526a47d06fa7e3ae21d21fa826893c60b72a
Saurabh Sharma <165271762+Saurabh-Sharma24@users.noreply.github.com>
2024-04-28 10:03:49 +0000

    arrow function

--- 03-basics/03-arrow-function.js
+++ 03-basics/03-arrow-function.js
@@ -1,4 +1,4 @@
-const user ={
+const user = { 
     name: "saurabh",
     welcomeMessage: function(){ // referencing current context with this keyword
         console.log(`${this.name}, welcome to website`) // this.name allows to us name property of user object via this keyword only whithin the context where object is defined
@@ -8,4 +8,21 @@ const user ={
 }
 
 console.log(user.welcomeMessage())
-console.log(this)// here this will return global context object which is empty
+console.log(this)// here this will return global context object which is empty(in node environment), this will returns window object in browser
+
+function fun(){
+    username = 'Saurabh'
+    console.log(this.username) // returns underined as this is only works whithin object context
+}
+
+console.log(fun()) // undefined
+
+const funnyFunction = () => { // arrow function
+    return `Hello`
+}
+
+const arrowFun = (num1,num2) => (num1+num2) //(implicit return) single expression doesn't require return keyword, we can use () but () also not required
+console.log(arrowFun(1,2))
+
+const returnObject = () => ({username: 'Saurabh', id: 12}) // always wrap object inside () while using arrow function
+console.log(returnObject())
