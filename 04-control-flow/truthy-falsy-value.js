baa92735b36d88c0ce83e32d16dc04733ac65954
Saurabh Sharma <165271762+Saurabh-Sharma24@users.noreply.github.com>
2024-04-28 17:41:00 +0000

    ?? operator

--- 04-control-flow/truthy-falsy-value.js
+++ 04-control-flow/truthy-falsy-value.js
@@ -10,4 +10,11 @@ if(user){
 }
 
 
-// nullish coalescing operator (??): null, undefined
+// nullish coalescing operator (??): null, undefined => used to check between two values
+
+let value;
+// value = 10 ?? 20; //always returns first value if none of them is null or undefined; output: 10
+value = null ?? 10; // always returns value except null; output: 10
+value = undefined ?? 20 ?? 122 // always returns first value if it isn't null or undefined ; output: 20
+value = null ?? undefined ?? 1 // output: 1
+console.log(value)
