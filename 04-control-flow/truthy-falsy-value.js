08e0732d69034a56422310de36afc727b7f19588
Saurabh Sharma <165271762+Saurabh-Sharma24@users.noreply.github.com>
2024-04-28 17:43:29 +0000

    ternary operator

--- 04-control-flow/truthy-falsy-value.js
+++ 04-control-flow/truthy-falsy-value.js
@@ -10,11 +10,16 @@ if(user){
 }
 
 
-// nullish coalescing operator (??): null, undefined => used to check between two values
+// nullish coalescing operator (??): null, undefined => used to check between two values to avoid errors
 
 let value;
 // value = 10 ?? 20; //always returns first value if none of them is null or undefined; output: 10
-value = null ?? 10; // always returns value except null; output: 10
-value = undefined ?? 20 ?? 122 // always returns first value if it isn't null or undefined ; output: 20
-value = null ?? undefined ?? 1 // output: 1
-console.log(value)
+// value = null ?? 10; // always returns value except null; output: 10
+// value = undefined ?? 20 ?? 122 // always returns first value if it isn't null or undefined ; output: 20
+// value = null ?? undefined ?? 1 // output: 1
+// console.log(value)
+
+// ternary operator
+
+// console.log(`${value?1:3}`) // output => 3 as value is undefined 
+
