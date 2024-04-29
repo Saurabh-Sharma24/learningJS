10ae39c129ec485319cb7e74fe0e6127d99e14d8
Saurabh Sharma <165271762+Saurabh-Sharma24@users.noreply.github.com>
2024-04-29 10:28:00 +0000

    filter method

---
+++ 05-iteration/filter-map-reduce.js
@@ -0,0 +1,6 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+// filter => returns an array based on some condition specified in its callback
+
+const greaterThanFour = numbers.filter((num)=> num>4)
+console.log(greaterThanFour)
